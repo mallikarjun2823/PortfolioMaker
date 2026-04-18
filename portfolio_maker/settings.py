25bd8cc992aa7857b8f0b8426829de9346311c40
Mallikarjun2823 <srinivasmalli15@gmail.com>
2026-04-18 15:03:50 +0530

    feat: add static file settings for improved asset management

--- portfolio_maker/settings.py
+++ portfolio_maker/settings.py
@@ -175,7 +175,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR / 'media'
